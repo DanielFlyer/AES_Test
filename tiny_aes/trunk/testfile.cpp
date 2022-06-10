1a89e6966291fa69c2ed82d8863377465b5655ca
DanielFlyer <90113726+DanielFlyer@users.noreply.github.com>
2022-06-10 13:47:38 -0400

    Update testfile.cpp

--- tiny_aes/trunk/testfile.cpp
+++ tiny_aes/trunk/testfile.cpp
@@ -8,37 +8,14 @@ int main(int argc, char **argv) {
 
 	// Create an instance of our module under test
 	Vtest_aes_128 *test_aes_128 = new Vtest_aes_128;
-	while( !Verilated::gotFinish() ) {
-		rand_word_array(state, 128);
-        	rand_word_array(key, bit_num);
-		test_aes_128->clk = 1;
-		test_aes_128->state = state;
-		test_aes_128->key = key;
-		tb->eval();
-		tb->clk = 0;
-		tb->eval();
-    //std::cout << "cpu_state: "+tb->__Vdly__tb__DOT__u0__DOT__u0__DOT__cpu_state << std::endl;
-	} exit(EXIT_SUCCESS);
-}
-word rand_word() {
-    word w = 0;
-    for(int i=0; i<4; i++) {
-        word x = rand() & 255;
-        w = (w << 8) | x;
-    }
-    return w;
-}
-
-void rand_word_array(word w[], int bit_num) {
-    int word_num = bit_num / 32;
-    for(int i=0; i<word_num; i++)
-        w[i] = rand_word();
-}
-
-void print_verilog_hex(word w[], int bit_num) {
-    int byte_num = bit_num / 8;
-    byte *b = (byte *)w;
-    printf("%d'h", bit_num);
-    for(int i=0; i<byte_num; i++)
-        printf("%02x", b[i]);
+	state = 128'h3243f6a8_885a308d_313198a2_e0370734;
+        key   = 128'h2b7e1516_28aed2a6_abf71588_09cf4f3c;
+	test_aes_128->state = state;
+	test_aes_128->key = key;
+	test_aes_128->clk = 1;
+	tb->eval();
+	tb->clk = 0;
+	tb->eval();
+	state = 128'h00112233_44556677_8899aabb_ccddeeff;
+        key   = 128'h00010203_04050607_08090a0b_0c0d0e0f;
 }
