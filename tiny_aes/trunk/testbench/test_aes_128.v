2b3ce953ebaa477674ac3105b4b2a332d08b853c
DanielFlyer <90113726+DanielFlyer@users.noreply.github.com>
2022-06-13 09:26:08 -0400

    Update test_aes_128.v

--- tiny_aes/trunk/testbench/test_aes_128.v
+++ tiny_aes/trunk/testbench/test_aes_128.v
@@ -35,6 +35,9 @@ module test_aes_128(clk);
 
  	initial begin
 		out = 0;
+		state = 0;
+		key = 0;
+		@ (negedge clk);
  		state = 128'h3243f6a8_885a308d_313198a2_e0370734;
         	key   = 128'h2b7e1516_28aed2a6_abf71588_09cf4f3c;
 		if (out !== 128'h3925841d02dc09fbdc118597196a0b32)
