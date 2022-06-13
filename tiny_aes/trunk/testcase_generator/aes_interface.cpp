113d030fdda57f098706e6e4f4065ce048b0a41f
DanielFlyer <90113726+DanielFlyer@users.noreply.github.com>
2022-06-13 12:03:17 -0400

    Update aes_interface.cpp

--- tiny_aes/trunk/testcase_generator/aes_interface.cpp
+++ tiny_aes/trunk/testcase_generator/aes_interface.cpp
@@ -17,29 +17,15 @@ word interface_128(word state, word key){
 
 	// Create an instance of our module under test
 	Vaes_128 *aes_128 = new Vaes_128;
-  
-  aes_128->state = state;
-  aes_128->key = key;
+	
+	aes_128->state = state;
+	aes_128->key = key;
 	aes_128->clk = 1;
 	aes_128->eval();
 	aes_128->clk = 0;
 	return aes_128->out;
 }
 
-word interface_128(word state, word key){
-  // Initialize Verilators variables
-	Verilated::commandArgs(argc, argv);
-
-	// Create an instance of our module under test
-	Vaes_128 *aes_128 = new Vaes_128;
-  
-  aes_128->state = state;
-  aes_128->key = key;
-	aes_128->clk = 1;
-	aes_128->eval();
-	aes_128->clk = 0;
-	return aes_128->out;
-}
 
 word interface_192(word state, word key){
   // Initialize Verilators variables
@@ -47,9 +33,9 @@ word interface_192(word state, word key){
 
 	// Create an instance of our module under test
 	Vaes_128 *aes_192 = new Vaes_192;
-  
-  aes_192->state = state;
-  aes_192->key = key;
+	
+	aes_192->state = state;
+	aes_192->key = key;
 	aes_192->clk = 1;
 	aes_192->eval();
 	aes_192->clk = 0;
@@ -62,9 +48,9 @@ word interface_256(word state, word key){
 
 	// Create an instance of our module under test
 	Vaes_128 *aes_256 = new Vaes_256;
-  
-  aes_256->state = state;
-  aes_256->key = key;
+	
+	aes_256->state = state;
+	aes_256->key = key;
 	aes_256->clk = 1;
 	aes_256->eval();
 	aes_256->clk = 0;
