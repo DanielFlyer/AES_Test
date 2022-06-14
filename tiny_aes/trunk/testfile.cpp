9bb494818c85f4c1e3d7615b734990845d8ad8bd
DanielFlyer <dflyer@princeton.edu>
2022-06-14 10:18:16 -0400

    commented out testbench

--- tiny_aes/trunk/testfile.cpp
+++ tiny_aes/trunk/testfile.cpp
@@ -1,19 +1,22 @@
 #include <stdlib.h>
 #include <iostream>
-#include "verilated.h"
-#include "./obj_dir/Vtest_aes_128.h"
+//#include "verilated.h"
+//#include "./obj_dir/Vtest_aes_128.h"
 
 int main(int argc, char **argv) {
 	// Initialize Verilators variables
-	Verilated::commandArgs(argc, argv);
+	// Verilated::commandArgs(argc, argv);
 
 	// Create an instance of our module under test
-	Vtest_aes_128 *test_aes_128 = new Vtest_aes_128;
+	// Vtest_aes_128 *test_aes_128 = new Vtest_aes_128;
 
-	while(!Verilated::gotFinish()){
+	/*while(!Verilated::gotFinish()){
 		test_aes_128->clk = 1;
 		test_aes_128->eval();
 		test_aes_128->clk = 0;
 		test_aes_128->eval();
-	} exit(EXIT_SUCCESS);
+	} */
+	std::cout << "test";
+	exit(EXIT_SUCCESS);
+	
 }
