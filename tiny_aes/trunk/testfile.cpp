e6e2f1fe2b9e7026c503ae865081fbd060710e3f
DanielFlyer <dflyer@princeton.edu>
2022-06-23 11:24:10 -0400

    better test bench

--- tiny_aes/trunk/testfile.cpp
+++ tiny_aes/trunk/testfile.cpp
@@ -1,50 +1,53 @@
 #include <stdlib.h>
 #include <iostream>
-#include <chrono>
+#include <time.h>
 #include "verilated.h"
 #include "./obj_dir/Vaes_128.h"
 #include "./testcase_generator/gen_test_case.c"
 #include "./testcase_generator/aes.c"
 #define REV(n) ((n << 24) | (((n>>16)<<24)>>16) |  (((n<<16)>>24)<<16) | (n>>24))
 typedef unsigned int word;
+double verilatorTime;
+double cTime;
 
 void encrypt_128_key_expand_inline_no_branch(word state[], word key[]);
 void convert_verilog_hex(word w[], int bit_num);
 
+
 int main(int argc, char **argv) {
+	Verilated::commandArgs(argc, argv);
+    verilatorTime = 0;
+    cTime = 0;
+
 	for(int i = 0; i < atoi(argv[1]); i++){
 	//Generate test case
 	word state[4];
     word key[4];
 	word out[4];
+	clock_t start, end;
 	rand_word_array(state, 128);
     rand_word_array(key, 128);
-    printf("plaintext: ");
-    print_verilog_hex(state, 128);
-    printf("\n");
-    printf("key:       ");
-    print_verilog_hex(key, 128);
-	printf("\n");
+    // printf("plaintext: ");
+    // print_verilog_hex(state, 128);
+    // printf("\n");
+    // printf("key:       ");
+    // print_verilog_hex(key, 128);
+	// printf("\n");
+
 	std::copy(std::begin(state), std::end(state), std::begin(out));
 
 	//C Implementation 
-	auto start = std::chrono::steady_clock::now();
+	start = clock();
 	encrypt_128_key_expand_inline_no_branch(out, key);
-	auto stop = std::chrono::steady_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+	end = clock();
+    cTime += ((double) (end - start)) / CLOCKS_PER_SEC;
 
-	printf("ciphertext:");
-    print_verilog_hex(out, 128);
-    printf("\n");
-
-	std::cout << "Time taken by C Implementation: "
-          << duration.count() << " nanoseconds" << std::endl;
+	// printf("ciphertext:");
+    // print_verilog_hex(out, 128);
+    // printf("\n");
 
 
 	//Verilator simulation
-
-	//Initialize Verilators variables
-	Verilated::commandArgs(argc, argv);
 
 	//Create an instance of our module under test
 	Vaes_128 *aes_128 = new Vaes_128;
@@ -55,20 +58,21 @@ int main(int argc, char **argv) {
 	out[2] = REV(out[2]);
 	out[1] = REV(out[1]);
 	out[0] = REV(out[0]);
-	auto start2 = std::chrono::steady_clock::now();
+	start = clock();
 	for(int j = 0; j < 24; j++){
 		aes_128->clk = 1;
 		aes_128->eval();
 		aes_128->clk = 0;
 		aes_128->eval();
 	}
-	auto stop2 = std::chrono::steady_clock::now();
-	auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(stop2 - start2);
-	std::cout << "Time taken by Verilator Simulation: "
-         << duration2.count() << " microseconds" << std::endl;
-		printf("\n");
+	end = clock();
+    verilatorTime += ((double) (end - start)) / CLOCKS_PER_SEC;
 	}
+	std::cout << "Time taken by C Implementation: "
+          << cTime << " seconds" << std::endl;	
+	std::cout << "Time taken by Verilator Simulation: "
+         << verilatorTime << " seconds" << std::endl;
+	std::cout << 100 * (verilatorTime - cTime) / cTime << "% time save\n";
 
-	
 	exit(EXIT_SUCCESS);
 }
